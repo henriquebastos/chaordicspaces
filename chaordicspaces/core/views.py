75d8ebb4992b9d6a382376638b1beb5cf79418f1
Henrique Bastos <henrique@bastos.net>
2014-01-13 20:21:51 -0200

    Authentication flow adjustments

--- chaordicspaces/core/views.py
+++ chaordicspaces/core/views.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.conf import settings
 
@@ -6,6 +7,7 @@ from django.conf import settings
 def home(request):
     return render(request, 'core/home.html')
 
+@login_required
 def subscription_new(request):
     return render(request, 'core/subscription_new.html', {
         'MOIP_TOKEN': settings.MOIP_TOKEN,
--- chaordicspaces/urls.py
+++ chaordicspaces/urls.py
@@ -8,6 +8,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'chaordicspaces.core.views.home', name='home'),
     url(r'^subscription/new/$', 'chaordicspaces.core.views.subscription_new', name='subscription_new'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', kwargs={'next_page': '/'}, name='logout'),
     url(r'^admin/', include(admin.site.urls)),
     url('', include('social.apps.django_app.urls', namespace='social')),
 )
